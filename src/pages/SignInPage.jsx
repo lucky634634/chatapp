83f8a4f2819ca3f45d0b08a32f43f6908e061314
lucky634634 <khoa21102003@gmail.com>
2026-02-23 23:00:38 +0700

    Refactor SignIn and SignUp pages: update styles and structure; add signup form and auth services

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -4,16 +4,25 @@ import "./SignInPage.css";
 export default function SignInPage() {
     return <>
         <NavBar />
-        <form className="signin-form">
-            <div className="title">Sign In</div>
-            <input type="email" placeholder="Enter your email" className="text-input" />
-            <input type="password" placeholder="Enter your password" className="text-input" />
-            <a className="forgot-password" href="">Forgot password?</a>
-            <div>
-                <input type="checkbox" id="remember" />
-                <label htmlFor="remember">Remember me</label>
+        <div className="signin-container">
+            <form className="signin-form">
+                <div className="title">Sign In</div>
+                <input type="email" placeholder="Enter your email" className="text-input" />
+                <input type="password" placeholder="Enter your password" className="text-input" />
+                <a className="forgot-password" href="">Forgot password?</a>
+                <div>
+                    <input type="checkbox" id="remember" />
+                    <label htmlFor="remember">Remember me</label>
+                </div>
+                <button type="submit">Sign In</button>
+            </form>
+            <div>-- or --</div>
+            <div className="auth-services">
+                <button className="google-signin">Sign in with Google</button>
             </div>
-            <button type="submit">Sign In</button>
-        </form>
+            <div className="signup-link">
+                Don't have an account? <a href="/signup">Sign Up</a>
+            </div>
+        </div>
     </>
 }
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,29 @@
+import "./SignUpPage.css"
 import NavBar from "../components/NavBar";
 
 export default function SignUpPage() {
     return <>
         <NavBar />
+        <div className="signup-container">
+            <form className="signup-form">
+                <div className="title">Sign Up</div>
+                <label htmlFor="name">Name</label>
+                <input type="text" placeholder="Enter your name" className="text-input" />
+                <label htmlFor="email">Email</label>
+                <input type="email" placeholder="Enter your email" className="text-input" />
+                <label htmlFor="password">Password</label>
+                <input type="password" placeholder="Enter your password" className="text-input" />
+                <label htmlFor="confirm-password">Confirm Password</label>
+                <input type="password" placeholder="Confirm your password" className="text-input" />
+                <button type="submit">Sign Up</button>
+            </form>
+            <div>-- or --</div>
+            <div className="auth-services">
+                <button className="google-signup">Sign up with Google</button>
+            </div>
+            <div className="signin-link">
+                Already have an account? <a href="/signin">Sign In</a>
+            </div>
+        </div>
     </>
 }
