bf4d36e79ca984b1411db4bab0c7c9e242affa87
lucky634634 <khoa21102003@gmail.com>
2026-03-10 21:13:05 +0700

    Replace BrowserRouter with HashRouter for hash-based routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignInPage";
@@ -9,8 +9,8 @@ function App() {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
-				<Route path="/signin" element={<SignInPage />} />
-				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/#/signin" element={<SignInPage />} />
+				<Route path="/#/signup" element={<SignUpPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
