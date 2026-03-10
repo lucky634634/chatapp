db34498979441d5ab576e4b75168b33366fdf041
lucky634634 <khoa21102003@gmail.com>
2026-03-10 21:27:35 +0700

    Set basename for HashRouter to '/chatapp/' for proper routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import SignUpPage from "./pages/SignUpPage";
 
 function App() {
 	return (
-		<HashRouter>
+		<HashRouter basename="/chatapp/">
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/#/signin" element={<SignInPage />} />
